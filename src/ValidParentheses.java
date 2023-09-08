c4fffd970948e1808d996fabfbe19c050bd721f9
Jyeverson Madalozo <71305121+Jyeverson@users.noreply.github.com>
2023-09-08 18:37:52 -0300

    problem solved

---
+++ src/ValidParentheses.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()){
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (c == ')' && top != '(') {
+                    return false;
+                } else if (c == ']' && top != '[') {
+                    return false;
+                } else if (c == '}' && top != '{') {
+                    return false;
+                }
+            }
+
+        }
+
+        return stack.isEmpty();
+    }
+}
