fd5d3728296fb3322cc751aa6ac4eb7db6fe9921
Jyeverson Madalozo <71305121+Jyeverson@users.noreply.github.com>
2023-09-10 16:09:25 -0300

    2011. Final Value of Variable After Performing Operations

---
+++ src/JAVA/FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class FinalValueOfVariableAfterPerformingOperations {
+
+    public int finalValueAfterOperations(String[] operations) {
+        int X = 0;
+
+        for (String operation : operations) {
+            if (operation.equals("++X") || operation.equals("X++")) {
+                X++;
+            } else if (operation.equals("--X") || operation.equals("X--")) {
+                X--;
+            }
+        }
+
+        return X;
+    }
+
+}
